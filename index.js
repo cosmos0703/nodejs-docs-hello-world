6e2255c931b990b1748fffd708dd023e59c6e58b
inhye park <cosmos0703@gmail.com>
2020-11-30 01:07:41 +0000

    test2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, This is the CNCG last session!, This is cosmos branch");
+    response.end("Hello, This is the CNCG last session!, This is cosmos branch on my cosmos linux");
 });
 
 const port = process.env.PORT || 1337;
